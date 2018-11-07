1d7692182184129e74bebf2a6dfdac1c2b56bb44
StoneNN <510508772@qq.com>
2018-11-07 18:09:16 +0800

    2018.11.7  --- column必须放到render作用域内才可运行

--- src/components/List_1_C.js
+++ src/components/List_1_C.js
@@ -4,70 +4,7 @@ import { connect } from 'dva';
 
 import styles from './List_1_C.css';
 
-const columns=[
-    {
-      title:'牌号',
-      dataIndex:'cardNum',
-      align:"center",
-    },
-    {
-      title:'局况',
-      dataIndex:'vulnerability',
-      align:"center",
-    },
-    {
-      title:'庄家',
-      dataIndex:'declare',
-      align:"center" 
-    },
-    {
-      title:'定约',
-      dataIndex:'contract',
-      align:"center" 
-    },
-    {
-      title:'首攻',
-      dataIndex:'leader',
-      align:"center" 
-    },
-    {
-      title:'结果',
-      dataIndex:'result',
-      align:"center" 
-    },
-    {
-      title:'得分',
-      dataIndex:'points',
-      align:"center" 
-    },
-    {
-      title:'操作',
-      align:"center", 
-      render:(record)=>{
-        console.log('-----record------',record);
-        return(
-         <React.Fragment>
-          <Popconfirm
-            title='确认删除?'
-            okText="确认"
-            cancelText="取消"
-            onConfirm={()=>this.onDelete(record.id)}
-          >
-            <Button>删除</Button>
-          </Popconfirm>
-          <Popconfirm
-            title='确认编辑?'
-            okText="确认"
-            cancelText="取消"
-            onConfirm={()=>onModify(record.id)}
-          >
-            <Button>编辑</Button>
-          </Popconfirm>
-         </React.Fragment>
-        );
-      }
-    }
-  ];
+
 class RecordList extends Component{
     
 
@@ -79,7 +16,70 @@ class RecordList extends Component{
       });
     }
     render(){
-
+      const columns=[
+        {
+          title:'牌号',
+          dataIndex:'cardNum',
+          align:"center",
+        },
+        {
+          title:'局况',
+          dataIndex:'vulnerability',
+          align:"center",
+        },
+        {
+          title:'庄家',
+          dataIndex:'declare',
+          align:"center" 
+        },
+        {
+          title:'定约',
+          dataIndex:'contract',
+          align:"center" 
+        },
+        {
+          title:'首攻',
+          dataIndex:'leader',
+          align:"center" 
+        },
+        {
+          title:'结果',
+          dataIndex:'result',
+          align:"center" 
+        },
+        {
+          title:'得分',
+          dataIndex:'points',
+          align:"center" 
+        },
+        {
+          title:'操作',
+          align:"center", 
+          render:(record)=>{
+            console.log('-----record------',record);
+            return(
+             <React.Fragment>
+              <Popconfirm
+                title='确认删除?'
+                okText="确认"
+                cancelText="取消"
+                onConfirm={()=>this.onDelete(record.id)}
+              >
+                <Button>删除</Button>
+              </Popconfirm>
+              <Popconfirm
+                title='确认编辑?'
+                okText="确认"
+                cancelText="取消"
+                onConfirm={()=>onModify(record.id)}
+              >
+                <Button>编辑</Button>
+              </Popconfirm>
+             </React.Fragment>
+            );
+          }
+        }
+      ];
         return(
             <Table
                 dataSource={ this.props.lists }
